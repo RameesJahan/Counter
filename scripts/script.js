f612d9ff182973a9d73828f2ea9227bffa50846d
Ramees Jahan <rameesjahan37@gmail.com>
2022-09-02 00:21:57 +0530

    Second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 var a = 0;
 var iptA= document.getElementById("count-ipt");
+var anim_div = document.getElementById("anim-click");
 
 function count() {
   a++;
@@ -11,3 +12,20 @@ function clearAll() {
   iptA.value = a;
   console.log(iptA.value);
 }
+
+var count_btn = document.getElementById("count-btn");
+count_btn.addEventListener('click',function(e){
+   
+   let x = e.clientX - e.target.offsetLeft;
+   let y = e.clientY - e.target.offsetTop;
+   
+   let ripples = document.createElement('span');
+   ripples.style.left = x + 'px';
+   ripples.style.top = y + 'px';
+   this.appendChild(ripples);
+   
+   setTimeOut(() => {
+     ripples.remove();
+     console.log('ripples removed')
+   },1000);
+});
